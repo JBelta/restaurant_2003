fb92d44ab8675d5a3206bc1f606c0c6a38bb3fb1
James Belta <james.belta@gmail.com>
2020-03-18 13:40:22 -0600

    Iteration 1 and 2 done

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,1 +1,41 @@
+class Restaurant
+  attr_reader :time, :name, :dishes
 
+  def initialize(time, name)
+    @time = time
+    @name = name
+    @dishes = []
+  end
+
+  def add_dish(dish)
+    @dishes << dish
+  end
+
+  def dishes
+    p @dishes
+  end
+
+  def opening_time
+    p @time
+  end
+
+  def closing_time(hours)
+    current_time = @time.to_i
+    time_change = current_time + hours
+    time_change.to_s + (":00")
+  end
+end
+
+restaurant = Restaurant.new("10:00", "Fuel Cafe")
+
+restaurant1 = Restaurant.new("10:00", "Fuel Cafe")
+restaurant2 = Restaurant.new('16:00', 'Il Poggio')
+
+restaurant1.closing_time(8)
+restaurant2.closing_time(7)
+
+restaurant = Restaurant.new('16:00', 'Il Poggio')
+
+restaurant.add_dish("Burrata")
+restaurant.add_dish("Pizzetta")
+restaurant.add_dish("Ravioli")
